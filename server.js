1ebac1e8d8e0c3fc4e54fd75ad704d4b043b87e7
simplyeleza <techdocpcu@gmail.com>
2018-11-26 23:22:17 +0900

    Adding bulk data one

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./server/models/db');
 const app = express();
 const port = 3000;
+const seed =require('./server/models/seed/seed-db');
 
 // setup the Express middlware
 require('./server/middleware/middleware')(app);
@@ -13,7 +14,11 @@ require('./server/api')(app);
  //connect to DB then run server
 db.sequelize.sync({
   force: true
-}).then(() => {  
+})
+.then(()=>{
+  seed.insert();
+})
+.then(() => {  
   app.listen(port,() => {
       console.log('running server on port'+ port);
   })
