cfd90d6fed124e62f80d9fe46cda4560106561cd
simplyeleza <techdocpcu@gmail.com>
2018-12-03 21:11:12 +0900

    Display User Books

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -21,7 +21,12 @@ exports.allUsers = (req, res) => {
 exports.singleUser = (req, res) => {
   let id = req.params.id;
 
-  models.User.findById(id)
+  models.User.findById(id,{
+   include: [{
+     model:models.Book,as:'Reading',
+     attributes:['title','author']
+   }]
+  })
     
     .then(user => {
       res.json(user);
