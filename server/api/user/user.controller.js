4b25ca2fccf0b46216c8933d0fe4a35d40a033e2
simplyeleza <techdocpcu@gmail.com>
2018-12-03 20:31:26 +0900

    Single User Route

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -1,3 +1,33 @@
-exports.test = (req, res) => {
-  res.send('API user route works!');
+// exports.test = (req, res) => {
+//   res.send('API user route works!');
+// }
+
+'use strict'
+
+const models  = require('../../models/db');
+
+exports.allUsers = (req, res) => {
+  models.User.findAll()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).send(error);
+    })
+}
+
+
+exports.singleUser = (req, res) => {
+  let id = req.params.id;
+
+  models.User.findById(id)
+    
+    .then(user => {
+      res.json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).send(error);
+    })
 }
--- server/api/user/user.routes.js
+++ server/api/user/user.routes.js
@@ -2,6 +2,9 @@ const controller = require('./user.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', controller.test);
+// router.get('/', controller.test);
+
+router.get('/allUsers', controller.allUsers);
+router.get('/:id', controller.singleUser);
 
 module.exports = router;
