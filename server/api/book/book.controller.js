540c063c59225cc2ba22035647a11e693804a58a
simplyeleza <techdocpcu@gmail.com>
2018-12-03 20:43:02 +0900

    Save user book

--- server/api/book/book.controller.js
+++ server/api/book/book.controller.js
@@ -12,3 +12,20 @@ exports.allBooks =(req,res) => {
         res.status(404).send(error);
     })
 }
+
+
+exports.saveUserBook = (req, res) => {
+    let bookId = req.body.bookId;
+    let userId = req.body.userId;
+  
+    models.Book.findById(bookId).then((book) => {
+      book.addReader(userId);
+    })
+    .then(() => {
+      res.json({success: 'Success, book added for User!'});
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).send(error);
+    })
+  }
--- server/api/book/book.routes.js
+++ server/api/book/book.routes.js
@@ -4,7 +4,7 @@ const express =require ('express');
 const router =express.Router();
 
 router.get('/allBooks',controller.allBooks);
-
+router.post('/saveUserBook', controller.saveUserBook);
 
 
 module.exports =router;
